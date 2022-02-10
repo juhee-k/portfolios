869d33436fd9bc03feed426f551f799b475551bc
Juhee Kim <sayjh26@gmail.com>
2022-02-10 15:21:34 -0800

    initial commit

---
+++ build/precache-manifest.9099b75db5b7c5f49a06c6a40fd38b68.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ea01abcfe309063e89ff58968e38efb",
+    "url": "/portfolios/index.html"
+  },
+  {
+    "revision": "1d49ba8c1e1361929da8",
+    "url": "/portfolios/static/css/main.a107faa9.chunk.css"
+  },
+  {
+    "revision": "9eb29499172c7819bcc9",
+    "url": "/portfolios/static/js/2.a5a3ed17.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/portfolios/static/js/2.a5a3ed17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d49ba8c1e1361929da8",
+    "url": "/portfolios/static/js/main.7394d326.chunk.js"
+  },
+  {
+    "revision": "2bcf44867261812385c2",
+    "url": "/portfolios/static/js/runtime-main.3bb1caab.js"
+  },
+  {
+    "revision": "46d13f23a4eba9f1eaf90752548e15e4",
+    "url": "/portfolios/static/media/image.46d13f23.png"
+  },
+  {
+    "revision": "b72ca94b7f3b1cac754301f78b184347",
+    "url": "/portfolios/static/media/image10.b72ca94b.png"
+  },
+  {
+    "revision": "948e0742c6c05428ff6ee0e4824fa6ef",
+    "url": "/portfolios/static/media/image13.948e0742.png"
+  },
+  {
+    "revision": "8dcea3d787834a60a831a2793dc4575e",
+    "url": "/portfolios/static/media/image3.8dcea3d7.png"
+  },
+  {
+    "revision": "22918e68075e23c929615816d040249d",
+    "url": "/portfolios/static/media/image4.22918e68.png"
+  },
+  {
+    "revision": "e0dfa31a9807429b2268343d7c46ce12",
+    "url": "/portfolios/static/media/image8.e0dfa31a.png"
+  },
+  {
+    "revision": "77b50e5151c7c98974660c7c6a8fc106",
+    "url": "/portfolios/static/media/profile.77b50e51.jpg"
+  },
+  {
+    "revision": "d50734cb04788a5f8aca2c25ede6372e",
+    "url": "/portfolios/static/media/project001.d50734cb.png"
+  },
+  {
+    "revision": "2a253513ba3a1603536437b31d498cd7",
+    "url": "/portfolios/static/media/project002.2a253513.png"
+  },
+  {
+    "revision": "e59369f461fcaaf9770517ca8beb850f",
+    "url": "/portfolios/static/media/project003.e59369f4.png"
+  }
+]);
